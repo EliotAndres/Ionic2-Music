f0d03e364b434bb3c4edffb781d5a27b6971d808
EliotAndres <eliota@bamlab.fr>
2015-11-12 14:13:05 +0100

    Working on android

--- www/app/search-bar/search-bar.js
+++ www/app/search-bar/search-bar.js
@@ -6,24 +6,25 @@ import {Http} from 'angular2/http';
     templateUrl: 'app/search-bar/search-bar.html',
     directives:[FORM_DIRECTIVES, NgIf, NgFor],
     events: ['change'],
-    
+
 })
 export class SearchBar{
 	searchModel;
 	items;
-	constructor(public http: Http) { 
+	constructor(public http: Http) {
     	this.change = new EventEmitter();
   	}
-  	
+
   	logError(err) {
 	    console.error('There was an error: ', err);
   	}
-  	
+
     addTrack(id){
+      this.items = null;
 	    console.log('query', this.searchModel);
-        this.change.next({id:id});
+      this.change.next({id:id});
     }
-    
+
     searchTracks(){
 	    var url = 'http://api.soundcloud.com/tracks?q=' + this.searchModel + '&client_id=f615a58a237bb0435f9c7de57070cdf4&format=json&_status_code_map[302]=200';
         this.http.get(url)
@@ -33,6 +34,6 @@ export class SearchBar{
 	        err => this.logError(err),
 	        () => console.log('Authentication Complete')
 	      );
-	      
+
     }
 }
